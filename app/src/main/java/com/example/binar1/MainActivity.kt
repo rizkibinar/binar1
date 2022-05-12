85d212dfde1d5110499fdc148d1b632f8923e96c
Rizki <rizki.putra@stockbit.com>
2022-05-12 21:00:00 +0700

    tambah contoh penjumlahan 2 bilangan

--- app/src/main/java/com/example/binar1/MainActivity.kt
+++ app/src/main/java/com/example/binar1/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.binar1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -11,20 +13,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main) // untuk men set layout yang di pakai pada activity
+        setContentView(R.layout.penjumlahan_layout) // untuk men set layout yang di pakai pada activity
 
-        val textLupaPassword = findViewById<TextView>(R.id.txt_lupa_password) // cari view di layout dengan id txt_lupa_password dengan bentuk TextView
+//        val textLupaPassword = findViewById<TextView>(R.id.txt_lupa_password) // cari view di layout dengan id txt_lupa_password dengan bentuk TextView
+//
+//        val imgSupport = findViewById<ImageView>(R.id.img_support)
+//
+//        imgSupport.setImageResource(R.drawable.ic_baseline_4g_mobiledata_24)
+//
+//        println("text lupa ${textLupaPassword.text}")
+//
+//        // ganti text TextView "programically"
+//        textLupaPassword.text = "Text lupa yang baru"
+//
+//        textLupaPassword.textAlignment = View.TEXT_ALIGNMENT_CENTER
 
-        val imgSupport = findViewById<ImageView>(R.id.img_support)
+        val angkaPertama = findViewById<EditText>(R.id.edit_angka_pertama)
+        val angkaKedua = findViewById<EditText>(R.id.edit_angka_kedua)
 
-        imgSupport.setImageResource(R.drawable.ic_baseline_4g_mobiledata_24)
+        val txtHasil = findViewById<TextView>(R.id.txt_hasil)
 
-        println("text lupa ${textLupaPassword.text}")
+        val btnHitung = findViewById<Button>(R.id.btn_hitung)
 
-        // ganti text TextView "programically"
-        textLupaPassword.text = "Text lupa yang baru"
+        btnHitung.setOnClickListener {
 
-        textLupaPassword.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            val num1 = angkaPertama.text.toString().toInt()
+            val num2 = angkaKedua.text.toString().toInt()
+
+            val hasil = num1 + num2
+
+            txtHasil.text = "Hasil Perhitungan : ${hasil.toString()}"
+
+
+            println("button hitung di click")
+        }
+
+
+
 
     }
 }

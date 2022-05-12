0e8d61d5915cf0cdbedcaef2068a67fb23a2ae30
Rizki <rizki.putra@stockbit.com>
2022-05-12 20:29:16 +0700

    add xml init

--- app/src/main/java/com/example/binar1/MainActivity.kt
+++ app/src/main/java/com/example/binar1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.binar1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_main) // untuk men set layout yang di pakai pada activity
+
+        val textLupaPassword = findViewById<TextView>(R.id.txt_lupa_password) // cari view di layout dengan id txt_lupa_password dengan bentuk TextView
+
+        val imgSupport = findViewById<ImageView>(R.id.img_support)
+
+        imgSupport.setImageResource(R.drawable.ic_baseline_4g_mobiledata_24)
+
+        println("text lupa ${textLupaPassword.text}")
+
+        // ganti text TextView "programically"
+        textLupaPassword.text = "Text lupa yang baru"
+
+        textLupaPassword.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
     }
 }
